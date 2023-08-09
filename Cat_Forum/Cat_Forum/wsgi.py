b1a9a6b5d02ffdbd65e8503d996c752f71f12acf
Shamil65 <shamilziganshin132@gmail.com>
2023-08-09 21:38:12 +0500

    init

---
+++ Cat_Forum/Cat_Forum/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Cat_Forum project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cat_Forum.settings')
+
+application = get_wsgi_application()
