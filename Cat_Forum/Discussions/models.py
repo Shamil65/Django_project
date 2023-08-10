0e8803e22323c447ae8cd93215188a6a1737f2a8
Shamil65 <shamilziganshin132@gmail.com>
2023-08-10 20:27:17 +0500

    Создание модели Cats и миграция

--- Cat_Forum/Discussions/models.py
+++ Cat_Forum/Discussions/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Cats(models.Model):
+    img = models.ImageField('Изображение кота')
+    publication_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    description = models.TextField(max_length=1000)
